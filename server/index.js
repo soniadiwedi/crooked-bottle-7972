7f6047b29e4428631336e68a1efe96663fe3dcb2
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2023-04-11 01:03:03 +0530

    checking1

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const routes = require("./controllers/user.routes");
 
 const app=express()
 require("dotenv").config()
+
 app.use(cors())
 
 app.use(express.json())
@@ -15,13 +16,8 @@ app.use(express.json())
 
 app.use("/user",routes)
 // app.use("/product",productRouter)
-app.listen(process.env.PORT||3000,async()=>{
-    try{
-        await connection
-        console.log("server connected to db");
-    }catch(err){
-        console.log(err);
-    }
+app.listen(process.env.PORT||3000,()=>{
+    connection()
     console.log(`server is running ${process.env.PORT||3000}`);
 })
 
